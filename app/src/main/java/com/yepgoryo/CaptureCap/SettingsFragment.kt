68a55b1ec8c0473a393d1ad8d721b663b53a2e5a
yepgoryo <yepgoryo@gmail.com>
2026-08-03 14:12:02 +0700

    Quickfix: rotation setting requires Android 16+

--- app/src/main/java/com/yepgoryo/CaptureCap/SettingsFragment.kt
+++ app/src/main/java/com/yepgoryo/CaptureCap/SettingsFragment.kt
@@ -31,7 +31,6 @@ class SettingsFragment : PreferenceFragmentCompat() {
             val preferenceFindPreference11: Preference = findPreference("drawoverlayerasevertical")!!
             val preferenceFindPreference12: Preference = findPreference("soundcontrolnotification")!!
             val preferenceFindPreference13: Preference = findPreference("screenorientation")!!
-            val preferenceFindPreference14: Preference = findPreference("screenrotation")!!
 
             val preferenceCategory2: PreferenceCategory = findPreference("capturesettings")!!
             preferenceCategory2.removePreference(preferenceFindPreference5)
@@ -43,7 +42,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
             preferenceCategory2.removePreference(preferenceFindPreference11)
             preferenceCategory2.removePreference(preferenceFindPreference12)
             preferenceCategory2.removePreference(preferenceFindPreference13)
-            preferenceCategory2.removePreference(preferenceFindPreference14)
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.BAKLAVA) {
+            val preferenceFindPreference14: Preference = findPreference("screenrotation")!!
+            val preferenceCategory4: PreferenceCategory = findPreference("capturesettings")!!
+            preferenceCategory4.removePreference(preferenceFindPreference14)
         }
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
             val preferenceFindPreference15: Preference = findPreference("enablevibration")!!
