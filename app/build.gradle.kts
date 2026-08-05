6c327aacaa83595dafe653c20413f3937193229e
yepgoryo <yepgoryo@gmail.com>
2026-08-05 19:56:09 +0700

    New release 3.9.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.yepgoryo.CaptureCap"
         minSdk = 23
         targetSdk = 36
-        versionCode = 37
-        versionName = "3.9.5.1"
+        versionCode = 38
+        versionName = "3.9.6"
 
         vectorDrawables.useSupportLibrary = true
 
