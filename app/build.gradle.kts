cebea05acec2243a501213bf328075ee35576e62
warptr <warptr@users.noreply.github.com>
2026-09-02 23:00:01 +0800

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.warptr.ShiShiRecorder"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
